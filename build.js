b57394e46e8efd899c2eee6a41cb32d3af14cbdf
Titus Wormer <tituswormer@gmail.com>
2022-11-23 13:50:44 +0100

    Refactor build script

--- build.js
+++ build.js
@@ -1,46 +1,45 @@
-import fs from 'node:fs'
-import https from 'node:https'
-import {bail} from 'bail'
-import concatStream from 'concat-stream'
+/**
+ * @typedef Info
+ * @property {Array<number>} codepoints
+ * @property {string} characters
+ *
+ * @typedef {Record<string, Info>} Result
+ */
+
+import fs from 'node:fs/promises'
+import {fetch} from 'undici'
 
 const own = {}.hasOwnProperty
 
-https.get('https://html.spec.whatwg.org/entities.json', (response) => {
-  response.pipe(concatStream(onconcat)).on('error', bail)
-})
+const response = await fetch('https://html.spec.whatwg.org/entities.json')
+const text = await response.text()
 
-/**
- * @param {Buffer} buf
- */
-function onconcat(buf) {
-  /** @type {Record<string, {codepoints: number[], characters: string}>} */
-  const data = JSON.parse(String(buf))
-  /** @type {Record<string, string>} */
-  const entities = {}
-  /** @type {string} */
-  let key
+/** @type {Result} */
+const data = JSON.parse(String(text))
+/** @type {Record<string, string>} */
+const entities = {}
+/** @type {string} */
+let key
 
-  for (key in data) {
-    if (own.call(data, key)) {
-      const name = key.slice(
-        1,
-        key.charAt(key.length - 1) === ';' ? -1 : undefined
-      )
-      entities[name] = data[key].characters
-    }
+for (key in data) {
+  if (own.call(data, key)) {
+    const name = key.slice(
+      1,
+      key.charAt(key.length - 1) === ';' ? -1 : undefined
+    )
+    entities[name] = data[key].characters
   }
-
-  fs.writeFile(
-    'index.js',
-    [
-      '/**',
-      ' * Map of named character references.',
-      ' *',
-      ' * @type {Record<string, string>}',
-      ' */',
-      'export const characterEntities = ' + JSON.stringify(entities, null, 2),
-      ''
-    ].join('\n'),
-    bail
-  )
 }
+
+await fs.writeFile(
+  'index.js',
+  [
+    '/**',
+    ' * Map of named character references.',
+    ' *',
+    ' * @type {Record<string, string>}',
+    ' */',
+    'export const characterEntities = ' + JSON.stringify(entities, null, 2),
+    ''
+  ].join('\n')
+)
