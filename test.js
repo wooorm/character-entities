fad5441451e83d486830052128030540cae075d5
Titus Wormer <tituswormer@gmail.com>
2023-06-03 16:13:49 +0200

    Add tests for exposed identifiers

--- test.js
+++ test.js
@@ -2,7 +2,13 @@ import assert from 'node:assert/strict'
 import test from 'node:test'
 import {characterEntities} from './index.js'
 
-test('characterEntities', function () {
+test('characterEntities', async function () {
+  assert.deepEqual(
+    Object.keys(await import('./index.js')).sort(),
+    ['characterEntities'],
+    'should expose the public api'
+  )
+
   assert.equal(characterEntities.AElig, 'Æ')
   assert.equal(characterEntities.aelig, 'æ')
   assert.equal(characterEntities.amp, '&')
